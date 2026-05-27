6f5ad542a96ab54b2d9ebcc76018847208996fbf
ketilynk <ketysherek@gmail.com>
2026-05-27 17:13:44 -0300

    teste

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,6 @@ Route::get('/rematricula', [App\Http\Controllers\Aluno::class, 'rematricula']);
 Route::get('/cancelar-matricula', [App\Http\Controllers\Aluno::class, 'cancelar']);
 Route::get('/notas', [App\Http\Controllers\pai::class, 'notas']);
 Route::get('/chamada', [App\Http\Controllers\professor::class, 'chamada']);
-
 Route::get('/aluno/login', [App\Http\Controllers\contato::class, 'contato']);
 
 Route::get('/professor/login', [App\Http\Controllers\contato::class, 'contato']);
